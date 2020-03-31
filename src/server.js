9382c67650f54faabf04472fcd4fb84458b7d975
LESANF <nagong1000@naver.com>
2020-03-31 23:54:55 +0900

    ING

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import path from "path";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "static")));
 
 app.get("/", (req, res) => {
@@ -17,4 +19,6 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+io.on("connection", () => console.log("Somebody Connected"));
