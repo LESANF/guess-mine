8b02bbf20d4ee264eb8d080dc814c2763efaaf28
LESANF <nagong1000@naver.com>
2020-05-12 18:22:42 +0900

    docs: END

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,15 +1,16 @@
 const body = document.querySelector("body");
 
-const fireNotification = (text, color) => {
+const fireNotification = (text, color, textColor) => {
   const notification = document.createElement("div");
   notification.innerText = text;
   notification.style.backgroundColor = color;
+  notification.style.color = textColor;
   notification.className = "notification";
   body.appendChild(notification);
 };
 
 export const handleNewUser = ({ nickname }) =>
-  fireNotification(`${nickname} just joined!`, "rgb(0, 122, 255)");
+  fireNotification(`${nickname} 님이 입장하셨습니다 📌`, "#fff", "#25ab07");
 
 export const handleDisconnected = ({ nickname }) =>
-  fireNotification(`${nickname} just left!`, "rgb(255, 149, 0)");
+  fireNotification(`${nickname} 님이 퇴장하셨습니다 📌`, "#fff", "#ff0000");
